5757ba5596c520f51eb0ad7c195e16ceb16a50dd
Darkzz01 <zakquielvaldez@gmail.com>
2023-08-22 19:57:18 -0400

    me rendi

--- lib/design/pages/home.dart
+++ lib/design/pages/home.dart
@@ -10,12 +10,157 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Essential'),
-      ),
-      body: const Center(
-        child: Text('Home'),
+      backgroundColor: const Color.fromARGB(255, 40, 40, 40),
+      body: SizedBox(
+        height: size.height,
+        child: ListView(
+          children: [
+            const SizedBox(height: 20),
+            Row(
+              children: [
+                const SizedBox(width: 20),
+                Container(
+                  height: 60,
+                  width: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: const CircleAvatar(
+                    backgroundColor: Colors.black26,
+                    foregroundImage: NetworkImage(
+                        'https://i.pinimg.com/564x/0d/8e/f0/0d8ef0b1c86d67d7ad0493dbcc67e2c9.jpg'),
+                  ),
+                ),
+                const Spacer(),
+                const Text(
+                  'Activity',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Spacer(),
+                const SizedBox(
+                  height: 50,
+                  width: 90,
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.notifications,
+                        size: 40,
+                        color: Colors.white,
+                      ),
+                      SizedBox(width: 10),
+                      Icon(
+                        Icons.date_range_rounded,
+                        size: 40,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(width: 20),
+              ],
+            ),
+            const SizedBox(height: 30),
+            Row(
+              children: [
+                Container(
+                  width: size.width * 0.45,
+                  height: 120,
+                  margin: const EdgeInsets.symmetric(horizontal: 15),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color.fromARGB(255, 73, 73, 73),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.all(20),
+                        child: const Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Save',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              '-\$ 400.00',
+                              style: TextStyle(
+                                fontSize: 19,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              'Expenses this Mouth',
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.white70,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: size.width * 0.45,
+                  height: 120,
+                  margin: const EdgeInsets.symmetric(horizontal: 10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color.fromARGB(255, 73, 73, 73),
+                  ),
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: const Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Save',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              '-\$ 400.00',
+                              style: TextStyle(
+                                fontSize: 19,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              'Expenses this Mouth',
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.white70,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
