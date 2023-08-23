041c80c0e40d93db540b05d9765f891be49715cf
Justin Inoa <justin@odespra.com>
2023-08-22 20:38:45 -0400

    -

---
+++ lib/core/utils/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color backgroundColor = Color.fromRGBO(29, 28, 34, 1);
+const Color greenColor = Color.fromRGBO(192, 222, 222, 1);
+const Color purpleColor = Color.fromRGBO(230, 223, 241, 1);
+const Color cakeColor = Color.fromRGBO(243, 238, 234, 1);
+const Color pinkColor = Color.fromRGBO(242, 224, 224, 1);
--- lib/design/pages/home.dart
+++ lib/design/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:essential/core/utils/constants.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -12,154 +13,72 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 40, 40, 40),
+      backgroundColor: backgroundColor,
+      appBar: AppBar(
+        backgroundColor: backgroundColor,
+        title: const Text(
+          'Essential',
+          style: TextStyle(
+            color: Colors.white54,
+            fontSize: 22,
+          ),
+        ),
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.menu,
+            color: Colors.white54,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          ),
+        ],
+      ),
       body: SizedBox(
         height: size.height,
         child: ListView(
           children: [
             const SizedBox(height: 20),
-            Row(
-              children: [
-                const SizedBox(width: 20),
-                Container(
-                  height: 60,
-                  width: 60,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                  child: const CircleAvatar(
-                    backgroundColor: Colors.black26,
-                    foregroundImage: NetworkImage(
-                        'https://i.pinimg.com/564x/0d/8e/f0/0d8ef0b1c86d67d7ad0493dbcc67e2c9.jpg'),
-                  ),
-                ),
-                const Spacer(),
-                const Text(
-                  'Activity',
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const Spacer(),
-                const SizedBox(
-                  height: 50,
-                  width: 90,
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.notifications,
-                        size: 40,
-                        color: Colors.white,
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 120,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: greenColor,
                       ),
-                      SizedBox(width: 10),
-                      Icon(
-                        Icons.date_range_rounded,
-                        size: 40,
-                        color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(width: 15),
+                  Expanded(
+                    child: Container(
+                      height: 120,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: cakeColor,
                       ),
-                    ],
+                    ),
                   ),
-                ),
-                const SizedBox(width: 20),
-              ],
-            ),
-            const SizedBox(height: 30),
-            Row(
-              children: [
-                Container(
-                  width: size.width * 0.45,
-                  height: 120,
-                  margin: const EdgeInsets.symmetric(horizontal: 15),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: const Color.fromARGB(255, 73, 73, 73),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.all(20),
-                        child: const Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Save',
-                              style: TextStyle(
-                                fontSize: 16,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              '-\$ 400.00',
-                              style: TextStyle(
-                                fontSize: 19,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              'Expenses this Mouth',
-                              style: TextStyle(
-                                fontSize: 15,
-                                color: Colors.white70,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  width: size.width * 0.45,
-                  height: 120,
-                  margin: const EdgeInsets.symmetric(horizontal: 10),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: const Color.fromARGB(255, 73, 73, 73),
-                  ),
-                  child: Column(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.all(10),
-                        child: const Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Save',
-                              style: TextStyle(
-                                fontSize: 16,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              '-\$ 400.00',
-                              style: TextStyle(
-                                fontSize: 19,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              'Expenses this Mouth',
-                              style: TextStyle(
-                                fontSize: 15,
-                                color: Colors.white70,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.large(
+        backgroundColor: const Color.fromARGB(255, 40, 40, 40),
+        onPressed: () {},
+        child: const Icon(
+          Icons.add,
+          size: 45,
+          color: Colors.white,
         ),
       ),
     );
